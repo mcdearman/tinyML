2e003a7054b48d4ea5e1ab8965393cae4a96206a
mcdearman <mcdearmanchris@gmail.com>
2024-09-19 22:16:38 -0500

    update

--- src/Infer.hs
+++ src/Infer.hs
@@ -1,1 +1,37 @@
 module Infer where
+
+import Control.Monad.State
+import Control.Placeholder (todo)
+import Data.Map (Map)
+import Data.Text
+import qualified NIR as N
+import Spanned
+import Ty
+
+data Solver = Solver
+  { constraints :: [Constraint],
+    subst :: Subst,
+    ctx :: Context
+  }
+
+data Constraint = Eq Ty Ty
+
+type Subst = Map TyVar Ty
+
+newtype Context = Context [Map Text Scheme]
+
+data Scheme = Scheme [TyVar] Ty
+
+type InferState a = State Solver a
+
+genConstraints :: Spanned N.Program -> InferState ()
+genConstraints p = todo
+
+genModuleConstraints :: Spanned N.Module -> InferState ()
+genModuleConstraints m = todo
+
+genDeclConstraints :: Spanned N.Decl -> InferState ()
+genDeclConstraints d = todo
+
+genExprConstraints :: Spanned N.Expr -> InferState ()
+genExprConstraints e = todo
